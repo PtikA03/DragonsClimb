695f69a3bf2927f5d37a73215553c5f1f1cee519
Pratik Anneboina <pratikanneboina@yahoo.com>
2026-01-29 09:38:55 -0700

    dragonsclimb_artifact2_05m

---
+++ dragons-climb/scripts/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed := 250.0
+@export var jump_force := -420.0
+@export var gravity := 1200.0
+
+
+var facing := 1
+var invulnerable := false
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	var direction := 0
+	if Input.is_action_pressed("move_left"):
+		direction -= 1
+	if Input.is_action_pressed("move_right"):
+		direction += 1		
